561d098bce3ec34128dfbcd39d5a8cc8483cc81e
KTH <pni2396@gmail.com>
2026-02-06 14:26:22 +0900

    chore: kakao login callback url permit

--- src/main/java/com/deepflow/settlementsystem/security/SecurityConfig.java
+++ src/main/java/com/deepflow/settlementsystem/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
                         .requestMatchers(
                                 "/api/auth/**",
+                                "/v1/oauth/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
